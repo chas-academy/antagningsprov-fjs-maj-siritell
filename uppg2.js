20e5eed1e49311dfda06ebf5324653ca87a073ea
siri.tell@hotmail.com <siri.tell@hotmail.com>
2025-05-30 16:15:14 +0200

    Löst uppgift 1-7

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,12 @@
-
 function uppg2() {
+
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
-
-
+   
+   let namn = "Siri Tell";
+   console.log(namn);
 }
+
+uppg2();
 
 module.exports = { uppg2 };
--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,14 @@
-
-
 function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
- 
+    let names = ["Siri", "Sarah", "Maria", "Moa"];
+
+    for (let name of names)
+        console.log(name);
 }
+
+uppg6();
 
 module.exports = { uppg6 };
